53d6b6a5b9bc30a0ce6e8705f306924374ddcf8c
Steve Markgraf <steve@steve-m.de>
2013-05-01 21:42:19 +0200

    update installation script, autodetect path
    Signed-off-by: Steve Markgraf <steve@steve-m.de>

--- dab_install.sh
+++ dab_install.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
+# Installation script for DAB receiver in Wine
+# https://github.com/steve-m/rtltcpaccess
 
-########################################################################
-# Note: Some older Wine installations use "C:\Program Files" instead   #
-# of "C:\Program Files (x86)", you might need to change that!          #
-########################################################################
-
-# run a dummy wine command to make sure installation is initialized
-winepath
+# figure out application path
+app_path="`wine cmd /c echo %ProgramFiles% | tr -d '\r\n'`\NOXON Media\NOXON DAB MediaPlayer"
 
 # grab the software
 wget http://ftp.terratec.de/NOXON/NOXON_DAB_Stick/Updates/NOXON_DAB_Stick_DAB_MediaPlayer_Setup_4.1.0.exe -O dab_setup.exe
@@ -15,17 +12,17 @@ wget http://ftp.terratec.de/NOXON/NOXON_DAB_Stick/Updates/NOXON_DAB_Stick_DAB_Me
 wine dab_setup.exe /S /v/qn
 
 # Updater can make trouble with some versions of Wine, remove it (running in background with 100% CPU usage)
-rm "`winepath -u "C:\Program Files (x86)\NOXON Media\NOXON DAB MediaPlayer\UpdateCheck.exe"`"
-rm "`winepath -u "C:\Program Files (x86)\NOXON Media\NOXON DAB MediaPlayer\RTL283XACCESS.dll"`"
+rm "`winepath -u "$app_path\UpdateCheck.exe"`"
+rm "`winepath -u "$app_path\RTL283XACCESS.dll"`"
 
 wget http://steve-m.de/projects/rtl-sdr/rtltcpaccess.tar.gz -O rtltcpaccess.tar.gz
 
 tar xvf rtltcpaccess.tar.gz
-cp RTL283XACCESS.dll "`winepath -u "C:\Program Files (x86)\NOXON Media\NOXON DAB MediaPlayer\"`"
+cp RTL283XACCESS.dll "`winepath -u "$app_path\"`"
 
 # Add device information to registry to make it believe a device is connected via USB
 wine regedit device_key.reg
 
 echo "Installation finished!"
 echo "to run it, start rtl_tcp first, and then start:"
-echo 'wine "`winepath -u "C:\Program Files (x86)\NOXON Media\NOXON DAB MediaPlayer\NOXON DAB MediaPlayer.exe"`"'
+echo "wine \"\`winepath -u \"$app_path\NOXON DAB MediaPlayer.exe\"\`"\"

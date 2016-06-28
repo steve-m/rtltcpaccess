078c9030b777609d1af325894058b085c7e96169
Steve Markgraf <steve@steve-m.de>
2016-06-28 03:13:46 +0200

    update install script to version 5.01 of the DAB application
    Signed-off-by: Steve Markgraf <steve@steve-m.de>

--- dab_install.sh
+++ dab_install.sh
@@ -2,14 +2,14 @@
 # Installation script for DAB receiver in Wine
 # https://github.com/steve-m/rtltcpaccess
 
-dab_md5="08596ea091409275c1350a7fa0e4a86d"
+dab_md5="a25b2303badbea40df5e2e2ff58486a3"
 tcpacc_md5="73a6f0eab866340b7a8389e95c77e757"
 
 get_files()
 {
-	if [ "`md5sum dab_setup.exe | cut -d ' ' -f 1`" != "$dab_md5" ]; then
-		wget http://ftp.noxonradio.de/NOXON/NOXON_DAB_Stick/Updates/NOXON_DAB_Stick_DAB_MediaPlayer_Setup_4.1.0.exe -O dab_setup.exe
-		[ "`md5sum dab_setup.exe | cut -d ' ' -f 1`" != "$dab_md5" ] && return 1
+	if [ "`md5sum DABPlayer5.01.zip | cut -d ' ' -f 1`" != "$dab_md5" ]; then
+		wget http://ftp.noxonradio.de/NOXON/NOXON_DAB_Stick/Updates/DABPlayer5.01.zip -O DABPlayer5.01.zip
+		[ "`md5sum DABPlayer5.01.zip | cut -d ' ' -f 1`" != "$dab_md5" ] && return 1
 	fi
 
 	if [ "`md5sum rtltcpaccess.tar.gz | cut -d ' ' -f 1`" != "$tcpacc_md5" ]; then
@@ -27,8 +27,11 @@ if [ "$?" != "0" ]; then
 fi
 
 # figure out application path
-app_path="`wine cmd /c echo %ProgramFiles% | tr -d '\r\n'`\NOXON Media\NOXON DAB MediaPlayer"
+app_path="`wine cmd /c echo %ProgramFiles% | tr -d '\r\n'`\NOXON\DAB Media Player"
 unix_path="`winepath -u "$app_path"`"
+
+mkdir /tmp/dab_install
+cd /tmp/dab_install/
 
 # grab the software
 get_files
@@ -37,11 +40,12 @@ if [ "$?" != "0" ]; then
 	exit 1
 fi
 
-# install it silently
-wine dab_setup.exe /S /v/qn
+# unpack the archive
+unzip -o DABPlayer5.01.zip
 
-# Updater can make trouble with some versions of Wine, remove it (running in background with 100% CPU usage)
-rm "$unix_path/UpdateCheck.exe"
+# install it silently
+wine msiexec /i DABStickInstaller5.01.msi /qn
+
 rm "$unix_path/RTL283XACCESS.dll"
 
 tar xvf rtltcpaccess.tar.gz
@@ -50,5 +54,17 @@ cp RTL283XACCESS.dll "$unix_path/"
 # Add device information to registry to make it believe a device is connected via USB
 wine regedit device_key.reg
 
+# Disable sending of usage statistics by setting registry variables
+# Normally there appears a dialog box that asks whether to disable this
+# when first starting the application, but starting with version 5 this
+# seems to trigger a Wine bug where the dialog box can't be closed.
+
+echo "REGEDIT4\n" > dab_settings.reg
+echo "[HKEY_CURRENT_USER\Software\Fraunhofer IIS\MultimediaPlayer\Settings\App]" >> dab_settings.reg
+echo "\"homecallActive\"=\"false\"" >> dab_settings.reg
+echo "\"homecallConfirmed\"=\"true\"" >> dab_settings.reg
+
+wine regedit dab_settings.reg
+
 echo "\nInstallation finished!\nTo run the software, start rtl_tcp first, and then start:"
-echo "wine \"\`winepath -u \"$app_path\NOXON DAB MediaPlayer.exe\"\`"\"
+echo "wine \"\`winepath -u \"$app_path\NOXON_DAB_MediaPlayer.exe\"\`"\"
